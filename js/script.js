3c4b9dd8057f97b21e18d2b3d3a05b8487c98c20
Andrii Avramenko <andreitvmm228@gmail.com>
2025-04-10 18:24:38 +0100

    Приклад масиву

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// const numbersList = [1,2,35,4,5,24,7,69]
+
+const fruits = ["Apple", "Pineapple", "Orange", "Banana", "Grape", "Venomfruit"];
+
+for (let i = 0; i < fruits.length; i += 1) {
+    console.log("Поточний фрукт:", fruits[i])
+}
+
+// console.log("Кількість елементів: " + numbersList.length)
+
+// for (let i = 1; i <= numbersList.length; i += 1) {
+//     console.log("Поточний елемент:", numbersList[i - 1])
+// }
