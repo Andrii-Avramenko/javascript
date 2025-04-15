6688e9105e30a37a1c53108b4f916335a3262dad
Andrii Avramenko <andreitvmm228@gmail.com>
2025-04-15 19:46:04 +0100

    Доопрацбвання дз 7

--- js/hws/hw-7.js
+++ js/hws/hw-7.js
@@ -4,7 +4,7 @@ numbersItems[1] = 10;
 
 // 2. Створити масив із трьох рядків. Додати до масиву ще одну рядків.
 let addMoreList = [7,"Chicken Jockey", 69];
-addMoreList[4] = "Flint and Steel";
+addMoreList[3] = "Flint and Steel";
 
 // 3. Створити скрипт який поверне суму всіх чисел в масиві.
 const addingList = [7,5,12,18];
@@ -28,8 +28,6 @@ for (let i = 0; i < stringList.length; i += 1) {
     if (stringList[i].length > 5) {
         console.log(`Речення ${stringList[i]} містить більше 5 символів`)
     };
-
-    continue
 };
 // 6. Створити масив з 10-ти чисел. Знайти та вивести на екран максимальне значення з масиву.
 const tenNumbers = [23, 5, 56, 102, 8, 7, 92, 43, 10, 86]
@@ -37,7 +35,7 @@ const highestNumber = Math.max(...tenNumbers)
 
 console.log("Найбільше число:", highestNumber)
 // 7. Створити масив з 10-ти чисел. Знайти всі парні числа в масиві та вивести їх на екран.
-for (let i = 0; i <= tenNumbers.length; i += 1) {
+for (let i = 0; i < tenNumbers.length; i += 1) {
     if (tenNumbers[i] % 2 !== 0) {
         continue;
     }
